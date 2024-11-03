f72b89b517be48aa8aac0a0df04d15c3a92c549f
ahmdmnt <ahmd.mnt@gmail.com>
2024-11-03 10:48:14 +0200

    Intro

---
+++ 01_Introduction-ModernCpp/00_FileStructure.cpp
@@ -0,0 +1,22 @@
+/**
+ * @brief Hello World Screen
+ */
+
+/* 1. Dependency */
+#include <iostream>
+
+/* 2. Preprocessor Directives */
+#define NAME "Ahmed"
+
+using namespace std;
+
+/* 3. Declarations */
+
+
+/* 4. Main, Code Entry Point*/
+int main() {
+
+    cout << "Hello " << NAME << "!" << endl;
+
+    return 0;
+}
